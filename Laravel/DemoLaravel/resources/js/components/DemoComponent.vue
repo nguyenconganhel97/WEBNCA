625c4eb2ee3ada58f7c863685cfec17d19c18c99
Cong Anh <nguyenconganhel97@gmail.com>
2021-04-26 07:44:08 +0700

    ca

---
+++ Laravel/DemoLaravel/resources/js/components/DemoComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Xin chao toi la Cong ANh</h2>
+    <h3>{{message}}</h3>
+    Sinh vien : {{student.name}}
+    <button v-on:click = "click()">Click type 1</button>
+    <button v-on:click = "click_name(student.name)">Click type 1</button>
+    {{name_student()}}
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: "Xin Chaoooo!",
+      student : {
+          name : "NCA",
+          age : 23
+      }
+    };
+  },
+  methods : {
+      click : function () {
+          alert(1);
+      },
+      click_name(name) {
+          alert("Name"+name)
+      },
+      name_student(){
+          return "Xin chao ban: "+this.student.name;
+      }
+
+
+  }
+};
+</script>
